e755f27f456e83bdd0e0316757c34ce1a280a22f
badamkac <Adam.kaczor.bb4@gmail.com>
2024-08-01 13:34:44 +0200

    refactor book

--- src/main/java/pl/adam/Book.java
+++ src/main/java/pl/adam/Book.java
@@ -1,44 +1,27 @@
 package pl.adam;
 
-import java.util.ArrayList;
-
 public class Book {
+    private final String title;
+    private final String author;
 
-    private String book;
-
-    public String getBook() {
-        return book;
+    public Book(String title, String author){
+        this.title = title;
+        this.author = author;
     }
 
-    ArrayList<String> bookList = new ArrayList<String>();
-
-    public void addBook(String bookName) {
-        bookList.add(bookName);
+    public String getTitle() {
+        return title;
     }
 
-    public void displayList() {
-        for (int i = 0; i < bookList.size(); i++) {
-            System.out.println(i + 1 + "." + " " + bookList.get(i));
-        }
+    public String getAuthor() {
+        return author;
     }
 
-    public void findBook(String bookName) {
-        boolean isThere = false;
-        for (String s : bookList) {
-            if (s.equalsIgnoreCase(bookName)) {
-                isThere = true;
-                break;
-            }
-        }
-        if (isThere){
-            System.out.println("Twoja książka jest w spisie");
-        }else{
-            System.out.println("Nie znaleziono książki o podanym tytule");
-        }
-//        if (bookList.contains(bookName)) {
-//            System.out.println("Twoja książka jest w spisie na miejscu " + (bookList.indexOf(bookName) + 1));
-//        } else {
-//            System.out.println("Nie znaleziono książki o podanym tytule");
-//        }
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
     }
 }
