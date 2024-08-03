ab7a2cdd7c44fc3cb66b3ed97afbbf5369b0a456
badamkac <Adam.kaczor.bb4@gmail.com>
2024-08-03 15:39:48 +0200

    refactor book

--- src/main/java/pl/adam/BookManager.java
+++ src/main/java/pl/adam/BookManager.java
@@ -1,19 +1,63 @@
 package pl.adam;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class BookManager {
 
     ArrayList<Book> bookList = new ArrayList<>();
 
-    public void addBook(String bookName, String author) {
-        Book book = new Book(bookName, author);
+    public void addBook(String bookName, String author, UUID isbn) {
+        Book book = new Book(bookName, author, isbn);
         bookList.add(book);
+    }
+
+    public void displayLastAddedBook(){
+        System.out.println(bookList.get(bookList.size() - 1));
     }
 
     public void displayList() {
         for (int i = 0; i < bookList.size(); i++) {
             System.out.println(i + 1 + "." + " " + bookList.get(i));
+        }
+    }
+
+    public void searchBookByTitle(String bookName) {
+        boolean isThere = false;
+        for (Book book : bookList) {
+            if (book.getTitle().equalsIgnoreCase(bookName)) {
+                System.out.println(book);
+                isThere = true;
+            }
+        }
+        if (!isThere){
+            System.out.println("Nie znaleziono książki o podanym tytule");
+        }
+    }
+
+    public void searchBookByAuthor(String author) {
+        boolean isThere = false;
+        for (Book book : bookList) {
+            if (book.getAuthor().equalsIgnoreCase(author)) {
+                System.out.println(book);
+                isThere = true;
+            }
+        }
+        if (!isThere){
+            System.out.println("W spisie nie ma ksiażek tego autora");
+        }
+    }
+
+    public void searchBookByIsbn(String isbn) {
+        boolean isThere = false;
+        for (Book book : bookList) {
+            if (book.getIsbn().toString().equalsIgnoreCase(isbn)) {
+                System.out.println(book);
+                isThere = true;
+            }
+        }
+        if (!isThere){
+            System.out.println("W spisie nie ma ksiażki o podanym ISBN");
         }
     }
 
@@ -30,10 +74,5 @@ public class BookManager {
         }else{
             System.out.println("Nie znaleziono książki o podanym tytule");
         }
-//        if (bookList.contains(bookName)) {
-//            System.out.println("Twoja książka jest w spisie na miejscu " + (bookList.indexOf(bookName) + 1));
-//        } else {
-//            System.out.println("Nie znaleziono książki o podanym tytule");
-//        }
     }
 }
--- src/main/java/pl/adam/BookManagerConsole.java
+++ src/main/java/pl/adam/BookManagerConsole.java
@@ -4,6 +4,7 @@ package pl.adam;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.UUID;
 
 public class BookManagerConsole {
     public static void main(String[] args) throws IOException {
@@ -12,13 +13,14 @@ public class BookManagerConsole {
 
         boolean programStart = true;
         while (programStart) {
-            System.out.println("Wybierz co chcesz zrobić: \n 1. Dodaj książkę \n 2. Wyświetl spis książek \n 3. Wyszukaj książkę po tytule \n 4. Zakończ");
+            System.out.println("Wybierz co chcesz zrobić: \n 1. Dodaj książkę \n 2. Wyświetl spis książek \n 3. Wyszukaj książkę po tytule \n 4. Wyszukaj książkę po autorze \n 5. Wyszukaj książkę po ISBN \n 6. Zakończ");
             String choice = reader.readLine();
             switch (choice) {
                 case "1":
-                    System.out.println("Wpisz tytuł książki:");
-                    bookManager.addBook(reader.readLine(), reader.readLine());
-                    System.out.println("Książka dodana poprawnie!");
+                    System.out.println("Wpisz tytuł a następnie autora książki:");
+                    bookManager.addBook(reader.readLine(), reader.readLine(),UUID.randomUUID());
+                    bookManager.displayLastAddedBook();
+                    System.out.println("Wprowadzona poprawnie");
                     break;
                 case "2":
                     System.out.println("Spis książek:");
@@ -26,13 +28,21 @@ public class BookManagerConsole {
                     break;
                 case "3":
                     System.out.println("Wpisz tytuł książki, której szukasz:");
-                    bookManager.findBook(reader.readLine(), reader.readLine());
+                    bookManager.searchBookByTitle(reader.readLine());
                     break;
                 case "4":
+                    System.out.println("Wpisz autora książki:");
+                    bookManager.searchBookByAuthor(reader.readLine());
+                    break;
+                case "5":
+                    System.out.println("Wpisz numer ISBN książki:");
+                    bookManager.searchBookByIsbn(reader.readLine());
+                    break;
+                case "6":
                     programStart = false;
                     break;
                 default:
-                    System.out.println("Błąd!  Wpisz na ekran cyfre od 1 - 4");
+                    System.out.println("Błąd!  Wpisz na ekran cyfre od 1 - 6");
                     break;
             }
         }
